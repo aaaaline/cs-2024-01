dd27b3e65d48c345653d87264c183db6e6525c41
Aline <alinelmcoelho@gmail.com>
2024-04-19 13:24:32 -0300

    Atualizando enumerações

--- endereco/src/main/java/com/github/aaaaline/cs_2024_01/endereco/TipoLogradouro.java
+++ endereco/src/main/java/com/github/aaaaline/cs_2024_01/endereco/TipoLogradouro.java
@@ -1,4 +1,12 @@
 package com.github.aaaaline.cs_2024_01.endereco;
 
 public enum TipoLogradouro {
+    Alameda,
+    Avenida,
+    Marginal,
+    Rua,
+    Rodovia,
+    Via,
+    Viela,
+    Travessa;
 }
